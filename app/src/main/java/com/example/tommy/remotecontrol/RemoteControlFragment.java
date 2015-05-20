1bf87df1d4a02faf40444f94c0daabc1e0cf7051
tommy-cheng <tommy.cfc.cheng@gmail.com>
2015-05-20 14:06:56 +0800

    Chapter 24, 1st version, P.398

--- app/src/main/java/com/example/tommy/remotecontrol/RemoteControlFragment.java
+++ app/src/main/java/com/example/tommy/remotecontrol/RemoteControlFragment.java
@@ -6,20 +6,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 /**
  * Created by tommy on 2015/5/19.
  */
 public class RemoteControlFragment extends Fragment {
-    private TextView mSelctedTextView;
+    private TextView mSelectedTextView;
     private TextView mWorkingTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_remote_control, parent, false);
 
-        mSelctedTextView = (TextView)v.findViewById(R.id.fragment_remote_control_selectedTextView);
+        mSelectedTextView = (TextView)v.findViewById(R.id.fragment_remote_control_selectedTextView);
         mWorkingTextView = (TextView)v.findViewById(R.id.fragment_remote_control_workingTextView);
 
         View.OnClickListener numberButtonListener = new View.OnClickListener() {
@@ -37,18 +39,39 @@ public class RemoteControlFragment extends Fragment {
             ;
         };
 
-        Button zeroButton = (Button)v.findViewById(R.id.fragment_remote_control_zeroButton);
+        TableLayout tableLayout = (TableLayout)v.findViewById(R.id.fragment_remote_control_tableLayout);
+        int number = 1;
+        for (int i =2; i<tableLayout.getChildCount()-1; i++) {
+            TableRow row = (TableRow)tableLayout.getChildAt(i);
+            for (int j=0; j<row.getChildCount(); j++) {
+                Button button = (Button)row.getChildAt(j);
+                button.setText("" + number);
+                button.setOnClickListener(numberButtonListener);
+                number++;
+            }
+        }
+
+        TableRow bottomRow = (TableRow)tableLayout.getChildAt(tableLayout.getChildCount()-1);
+
+        Button deleteButton = (Button)bottomRow.getChildAt(0);
+        deleteButton.setText("Delete");
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mWorkingTextView.setText("0");
+            }
+        });
+
+        Button zeroButton = (Button)bottomRow.getChildAt(1);
+        zeroButton.setText("0");
         zeroButton.setOnClickListener(numberButtonListener);
 
-        Button oneButton = (Button)v.findViewById(R.id.fragment_remote_control_oneButton);
-        oneButton.setOnClickListener(numberButtonListener);
-
-        Button enterButton = (Button)v.findViewById(R.id.fragment_remote_control_enterButton);
+        Button enterButton = (Button) bottomRow.getChildAt(2);
+        enterButton.setText("Enter");
         enterButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 CharSequence working = mWorkingTextView.getText();
                 if (working.length() > 0)
-                    mSelctedTextView.setText(working);
+                    mSelectedTextView.setText(working);
                 mWorkingTextView.setText("0");
             }
         });
